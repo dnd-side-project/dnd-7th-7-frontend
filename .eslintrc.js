7ef5a0eafc6ff8ff4dd5d22adbe3880a587f0896
kmjnnhyk <rlawlsgur620@gmail.com>
2022-07-14 16:09:02 +0900

    feat: 🎸 프로젝트 설정 완료
    'eslint 및 prettier 완료했고 노션에 세팅법 정리해뒀습니다.'

---
+++ .eslintrc.js
@@ -0,0 +1,29 @@
+module.exports = {
+  env: {
+    es2021: true,
+    node: true,
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    '@react-native-community',
+    'airbnb',
+    'airbnb/hooks',
+    'plugin:prettier/recommended',
+  ],
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  plugins: ['react', 'react-hooks'],
+  rules: {
+    indent: ['error', 2, { SwitchCase: 1 }],
+    'linebreak-style': ['error', 'unix'],
+    quotes: ['off', 'single'],
+    semi: ['error', 'always'],
+    'no-empty-function': 'off',
+  },
+};
