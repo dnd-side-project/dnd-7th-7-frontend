5ab9cc2241f4ec7de78785cc44826498a6954ee9
kmjnnhyk <rlawlsgur620@gmail.com>
2022-08-12 15:25:02 +0900

    🐛 fix: fonts setting 에러 해결
    expo-font의 useFonts 함수를 따로 utils의 getFonts함수로 빼서 정리

--- App.jsx
+++ App.jsx
@@ -1,16 +1,18 @@
 import React, { useEffect, useState } from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import * as SplashScreen from 'expo-splash-screen';
-import { useFonts } from 'expo-font';
 import OnBoarding from '@screens/OnBoarding';
 import BottomTabs from '@screens/BottomTabs';
+import * as SplashScreen from 'expo-splash-screen';
+import { useFonts } from 'expo-font';
 import { getFonts } from './hooks/utils';
 
 const Stack = createNativeStackNavigator();
 
 const App = () => {
-  getFonts();
+  let fontsLoaded = getFonts();
+  if (!fontsLoaded) return null;
+
   return (
     <NavigationContainer>
       <Stack.Navigator initialRouteName={'onboarding'}>
