2bac6648733a948b92d84ea24f14ab5757522e6b
kmjnnhyk <rlawlsgur620@gmail.com>
2022-08-18 16:43:54 +0900

    ⚙️ feat: bottom tab 네비게이터 디자인

---
+++ assets/images/navigator/index.js
@@ -0,0 +1,23 @@
+import BookMarkIcon from './BookMarkIcon.svg';
+import BookMarkIconActive from './BookMarkIconActive.svg';
+import HomeIcon from './HomeIcon.svg';
+import HomeIconActive from './HomeIconActive.svg';
+import MyPageIcon from './MyPageIcon.svg';
+import MyPageIconActive from './MyPageIconActive.svg';
+import RecordingIcon from './RecordingIcon.svg';
+import RecordingIconActive from './RecordingIconActive.svg';
+import SearchIcon from './SearchIcon.svg';
+import SearchIconActive from './SearchIconActive.svg';
+
+export {
+  BookMarkIcon,
+  BookMarkIconActive,
+  HomeIcon,
+  HomeIconActive,
+  MyPageIcon,
+  MyPageIconActive,
+  RecordingIcon,
+  RecordingIconActive,
+  SearchIcon,
+  SearchIconActive,
+};
