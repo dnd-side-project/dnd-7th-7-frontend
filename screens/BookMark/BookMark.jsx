633090598ced1a9491ffeb4c6f8ea0073cb8c570
Parkjju <rudwns3927@gmail.com>
2022-08-20 10:11:41 +0900

    style: 책갈피 페이지 제작

--- screens/BookMark/BookMark.jsx
+++ screens/BookMark/BookMark.jsx
@@ -1,14 +1,24 @@
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import React from 'react';
 import { StyleSheet, View, Text } from 'react-native';
-import * as S from './Bookmark.style';
+import Header from '../../components/Header';
+import BookMarkMain from './BookMarkMain/BookMarkMain';
+
+const BookMarkStack = createNativeStackNavigator();
 
 const BookMark = () => {
   return (
-    <View style={style.container}>
-      <View style={style.wrapper}>
-        <Text>Search</Text>
-      </View>
-    </View>
+    <BookMarkStack.Navigator>
+      <BookMarkStack.Screen
+        options={{
+          header: (
+            { navigation }, // 기본 navigation props도 사용 가능합니다
+          ) => <Header navigation={navigation} left="책갈피" right={null} />,
+        }}
+        component={BookMarkMain}
+        name="BookMarkMain"
+      ></BookMarkStack.Screen>
+    </BookMarkStack.Navigator>
   );
 };
 
