53ed6fb9a3c1b3db0916e1a78511e68e6f6301b9
Parkjju <rudwns3927@gmail.com>
2022-08-22 00:28:53 +0900

    style: 나의 리뷰 페이지 제작

---
+++ screens/MyPage/Review/Review.style.js
@@ -0,0 +1,16 @@
+import styled from 'styled-components/native';
+
+export const ReviewBox = styled.View`
+  width: 100%;
+  height: 300px;
+  padding: 0px 16px;
+  padding-bottom: 20px;
+  margin-bottom: 20px;
+`;
+export const ThumbnailBodyInformation = styled.View`
+  width: 100%;
+  height: 30px;
+  flex-direction: row;
+  align-items: center;
+  justify-content: space-between;
+`;
